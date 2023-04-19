9f8038395c06cd1e40140086cf771abae49d88f7
rafagomezUCR <rgome020@ucr.edu>
2023-04-18 19:38:19 -0700

    initial index of schedule screen is the current day

--- aniseason/lib/Screens/schedule_screen.dart
+++ aniseason/lib/Screens/schedule_screen.dart
@@ -1,15 +1,42 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 
 import '../Styles/appcolors.dart';
 
-class AnimeSchedule extends StatelessWidget {
-  const AnimeSchedule({super.key});
+class AnimeSchedule extends StatefulWidget {
+  AnimeSchedule({super.key});
+
+  @override
+  State<AnimeSchedule> createState() => _AnimeScheduleState();
+}
+
+class _AnimeScheduleState extends State<AnimeSchedule> {
+  late String _currentDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentDay = DateFormat('EEEE').format(DateTime.now());
+  }
+
+  Map<String, int> startingTabIndex = {
+    "Sunday": 0,
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6
+  };
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      initialIndex: startingTabIndex[_currentDay] ?? 0,
       length: 7,
       child: Scaffold(
         appBar: AppBar(
